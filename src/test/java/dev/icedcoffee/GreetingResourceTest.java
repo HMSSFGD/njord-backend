a3fca8638a4c1b494c41bdd0edb3c49fb9d2ee2d
danjones1618 <hello@danjones.dev>
2021-03-21 11:14:22 +0000

    Update ci

--- src/test/java/dev/icedcoffee/GreetingResourceTest.java
+++ src/test/java/dev/icedcoffee/GreetingResourceTest.java
@@ -11,11 +11,5 @@ public class GreetingResourceTest {
 
     @Test
     public void testHelloEndpoint() {
-        given()
-          .when().get("/hello-resteasy")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
     }
-
 }
